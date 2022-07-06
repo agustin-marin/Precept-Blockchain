f96a786b7d3257d405e8571559bdc58b90dd96b1
agustin-marin <60695790+agustin-marin@users.noreply.github.com>
2022-07-06 15:37:46 +0200

    created network for containers

--- hyperledger/crypto-config-source/scripts/generarFicherosTx.sh
+++ hyperledger/crypto-config-source/scripts/generarFicherosTx.sh
@@ -9,4 +9,4 @@ rm -rf ../../crypto-config  ../../channel-artifacts/
 ./configtxgen -profile  OneOrgOrdererGenesis  -outputBlock ../../channel-artifacts/genesis.block -channelID orderer-system-channel -configPath "$PWD/../"  # generar fichero genesis usado por orderer
 ./configtxgen -outputCreateChannelTx ../../channel-artifacts/mychannel.tx -profile OneOrgChannel -channelID mychannel -configPath "$PWD/../" # generar fichero .tx para crear el canal dentro del DLT
 
-./configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ../../channel-artifacts/Org1MSPanchor.tx -channelID channel -asOrg Org1MSP -configPath "$PWD/../" # fichero (innecesario en este caso) para canales multiorganizaciones
+#./configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ../../channel-artifacts/Org1MSPanchor.tx -channelID channel -asOrg Org1MSP -configPath "$PWD/../" # fichero (innecesario en este caso) para canales multiorganizaciones
