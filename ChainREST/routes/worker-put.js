bfbe70148fd67062118d61dc133abd6c891f2b1a
agustin-marin <60695790+agustin-marin@users.noreply.github.com>
2022-07-15 13:33:13 +0200

    solve ip error

--- ChainREST/routes/worker-put.js
+++ ChainREST/routes/worker-put.js
@@ -41,7 +41,7 @@ const conf = {
         },
         orderers: {
             "orderer.odins.com": {
-                url: "grpcs://10.208.211.22:7050",
+                url: "grpcs://orderer.odins.com:7050",
                 tlsCACerts: {
                     path:
                         "/usr/src/app/crypto-config/ordererOrganizations/odins.com/orderers/orderer.odins.com/msp/tlscacerts/tlsca.odins.com-cert.pem",
@@ -50,7 +50,7 @@ const conf = {
         },
         peers: {
             "peer0.org1.odins.com": {
-                "url": "grpcs://10.208.211.22:7051",
+                "url": "grpcs://peer0.org1.odins.com:7051",
                 tlsCACerts: {
                     path:
                         "/usr/src/app/crypto-config/peerOrganizations/org1.odins.com/peers/peer0.org1.odins.com/msp/tlscacerts/tlsca.org1.odins.com-cert.pem",
@@ -60,7 +60,7 @@ const conf = {
     },
     certificateAuthorities: {
         "ca.org1.odins.com": {
-            "url": "https://10.208.211.22:7054",
+            "url": "https://ca.org1.odins.com:7054",
             "httpOptions": {
                 "verify": false
             },
