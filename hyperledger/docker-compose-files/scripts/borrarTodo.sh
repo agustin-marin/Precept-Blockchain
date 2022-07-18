cae1973d5f4be683e09b1623c42f196a46a40592
agustin-marin <60695790+agustin-marin@users.noreply.github.com>
2022-07-18 10:43:08 +0200

    Update borrarTodo.sh

--- hyperledger/docker-compose-files/scripts/borrarTodo.sh
+++ hyperledger/docker-compose-files/scripts/borrarTodo.sh
@@ -8,4 +8,8 @@ docker-compose -f ../docker-compose-orderer-1.yml down -v
 
 docker-compose -f ../docker-compose-peer0-org1.yml down -v
 
+cd ../../../blockchain-explorer
+
+docker-compose down -v
+
 docker volume prune -f
